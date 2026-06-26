3e82749c103441ae8647e0b46095ce98aae7edef
tudor.pascu <tudor.pascu@booking.com>
2026-06-26 12:02:49 +0300

    log tile clicks

--- src/services/MapService.ts
+++ src/services/MapService.ts
@@ -15,6 +15,7 @@ export class MapService {
     }
 
     tileClicked(tile: Tile) {
+        console.log(tile);
         if (tile.player_building_id !== null) {
             const playerBuildings: PlayerBuilding[] = this.scene.registry.get('playerBuildings') || [];
             const playerBuilding = playerBuildings.find(b => b.id === tile.player_building_id);
